e97b2b5b683c16173e399c0afb5b7f963741af4e
aarshpatel1 <aarsh18patel@gmail.com>
2024-11-12 15:58:02 +0530

    final commit

---
+++ app/src/main/java/com/example/multiscreenapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.multiscreenapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonToSecond = findViewById(R.id.buttonToSecond);
+        Button buttonToThird = findViewById(R.id.buttonToThird);
+
+        buttonToSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonToThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
